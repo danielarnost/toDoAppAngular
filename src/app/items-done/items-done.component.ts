ba0ad42f5565a8a3a78d821fac7c66a0d8ab6814
danielarnost <danielarnost@utexaas.edu>
2017-01-15 17:02:32 -0600

    not working need app file

---
+++ src/app/items-done/items-done.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'items-done',
+  templateUrl: './items-done.component.html',
+  styleUrls: ['./items-done.component.css']
+})
+export class ItemsDoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
